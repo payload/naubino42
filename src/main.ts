d594faeb14491dc9507529fbb46a72afebf27692
Gilbert Röhrbein <gilbert@ifsr.de>
2017-09-19 21:57:49 +0200

    fix test Naub find_cycles finds no cycle in middle of naub chain

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,12 @@ let naubino: Naubino
 naubino = new Naubino()
 naubino.size = { x: 200, y: 200 }
 
-const test_cycle = naubino.create_naub_chain(3);
+const test_cycle = naubino.create_naub_chain(5);
 const reachable_a = test_cycle[0].reachable_naubs()
-assert.equal(reachable_a.length, 3, "not all naubs reachable")
+assert.equal(reachable_a.length, test_cycle.length, "not all naubs reachable")
 
-test_cycle[0].join_naub(test_cycle[test_cycle.length - 1])
 const reachable_b = test_cycle[0].reachable_naubs()
 assert.deepEqual(reachable_a, reachable_b, "reachable_naubs after join_naub differs")
 
-const cycles = test_cycle[0].find_cycles()
-assert.isNotEmpty(cycles, "no cycles found")
-
-console.log(test_cycle.length, cycles[0].length, cycles[0])
-assert.deepEqual(new Set(test_cycle), new Set(cycles[0]), "cycle didn't detect everyone")
+const cycles = test_cycle[2].find_cycles()
+assert.isEmpty(cycles, "cycles found")
