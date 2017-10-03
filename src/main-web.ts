14323131fa655e0618b92fe08e19544a61bcdd9a
Gilbert Röhrbein <gilbert@ifsr.de>
2017-10-03 18:04:40 +0200

    replace web hunter example with ArenaMode

--- src/main-web.ts
+++ src/main-web.ts
@@ -1,8 +1,9 @@
 import * as _ from "lodash"
 import { Vector } from "matter-js"
-import { Naubino, Naub, Hunter, Update } from "./naubino"
+import { Naubino, Naub, Hunter, Update, ArenaMode } from "./naubino"
 
 let naubino: Naubino
+let mode: ArenaMode
 let canvas: HTMLCanvasElement
 
 window.addEventListener("load", () => {
@@ -24,19 +25,15 @@ function canvas_init() {
 function naubino_init() {
     naubino = new Naubino()
     naubino.size = { x: 600, y: 400 }
+    mode = new ArenaMode(naubino)
 }
 
-let hunters = new Set<Hunter>()
 function naubino_start() {
-    const chain_a = naubino.create_naub_chain(100, { x: naubino.size.x / 2, y: naubino.size.y * 1 / 3 })
-    const chain_b = naubino.create_naub_chain(100, { x: naubino.size.x / 2, y: naubino.size.y * 2 / 3 })
-    const hunter_0 = new Hunter(naubino, chain_a[0], chain_b[10])
-    const hunter_1 = new Hunter(naubino, chain_a[chain_a.length - 1], chain_b[chain_b.length - 10])
-    hunters.add(hunter_0)
-    hunters.add(hunter_1)
+
 }
 
 function main_loop() {
+    mode.step()
     const update = naubino.step()
 
     const ctx = canvas.getContext("2d")
@@ -61,9 +58,6 @@ function main_loop() {
         ctx.fill()
     }
 
-    for (const hunter of hunters) {
-        hunter.step()
-    }
     window.requestAnimationFrame(() => {
         main_loop()
     })
