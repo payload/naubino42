0bdcf24a35446444bab6ec1e1981a0a674a5cbc3
Gilbert Röhrbein <gilbert@ifsr.de>
2017-10-08 19:30:21 +0200

    refactor PointerSystem and Pointer into pointer-system.ts

---
+++ src/pointer-system.ts
@@ -0,0 +1,119 @@
+import { Naubino, Naub } from "./naubino"
+import { Vector, Matter } from "./matter-js"
+
+import * as _ from "lodash"
+import { EventEmitter } from "eventemitter3"
+
+export class PointerSystem {
+
+    naubino: Naubino
+    engine: Matter.Engine
+    pointers = new Set<Pointer>()
+    map_naub_pointer = new Map<Pointer, Naub>();
+    ee = new EventEmitter()
+
+    constructor(naubino: Naubino, engine: Matter.Engine) {
+        this.naubino = naubino
+        this.engine = engine
+    }
+
+    // like pynaubino Naubino.touch_down
+    touch_down(pos: Vector) {
+        const naub = this.find_naub(pos)
+        if (naub) {
+            return this.connect_pointer_naub(naub)
+        }
+        return null
+    }
+
+    find_naub(pos: Vector): Naub {
+        const hits = Matter.Query.point(this.engine.world.bodies, pos)
+        for (const body of hits) {
+            for (const naub of this.naubino.naubs) {
+                if (naub.body == body) return naub
+            }
+        }
+        return null
+    }
+
+    // like pynaubino Naub.select
+    connect_pointer_naub(naub: Naub, pos?: Vector, pointer?: Pointer) {
+        console.assert(naub.alive)
+        if (!pos) pos = _.clone(naub.pos)
+        if (!pointer) pointer = this.naubino.create_pointer(pos)
+        pointer.constraint = Matter.Constraint.create({
+            bodyA: pointer.body,
+            bodyB: naub.body,
+            pointB: Matter.Vector.sub(pointer.pos, naub.body.position),
+            length: 2
+        })
+        Matter.World.add(this.engine.world, pointer.constraint)
+        this.map_naub_pointer.set(pointer, naub)
+        naub.pointers.add(pointer)
+        this.pointers.add(pointer)
+        return pointer
+    }
+
+    remove_pointer(pointer: Pointer) {
+        this.pointers.delete(pointer)
+        const naub = this.map_naub_pointer.get(pointer)
+        this.map_naub_pointer.delete(pointer)
+        if (naub) naub.pointers.delete(pointer)
+        Matter.World.remove(this.engine.world, pointer.constraint)
+        this.ee.emit("remove_pointer", pointer)
+    }
+
+    step() {
+        for (const pointer of this.pointers) {
+            pointer.step()
+        }
+        for (const pointer of this.pointers) {
+            if (!pointer.alive) this.remove_pointer(pointer)
+        }
+    }
+}
+
+
+export class Pointer {
+
+    body = Matter.Body.create({})
+    pos: Vector
+    constraint: Matter.Constraint
+    alive = true
+
+    constructor(pos: Vector) {
+        Matter.Body.setStatic(this.body, true)
+        Matter.Body.setPosition(this.body, pos)
+        this.pos = pos
+    }
+
+    remove() {
+        this.alive = false
+    }
+
+    up() {
+        this.remove()
+    }
+
+    move(to_move: Vector) {
+        Vector.add(this.pos, to_move, this.pos)
+    }
+
+    moveTo(pos: Vector) {
+        this.pos = pos
+    }
+
+    step() {
+        const body_pos = Vector_interpolate_to(this.body.position, this.pos, 1)
+        this.body.velocity = Matter.Vector.sub(body_pos, this.body.position)
+        this.body.position = body_pos
+    }
+}
+
+
+function Vector_interpolate_to(a: Vector, b: Vector, ratio: number, out?: Vector) {
+    const diff = Matter.Vector.sub(b, a)
+    const norm = Matter.Vector.normalise(diff)
+    const forward = Matter.Vector.mult(norm, ratio)
+    return Matter.Vector.add(a, forward, out)
+}
