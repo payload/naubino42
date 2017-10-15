e068cfa391843bc724529dd978e0c7c338953cc5
Gilbert Röhrbein <gilbert@ifsr.de>
2017-10-15 13:29:48 +0200

    WIP big tuning for naubino on Android

--- src/main-web.ts
+++ src/main-web.ts
@@ -16,9 +16,137 @@ window.addEventListener("load", () => {
     window.setTimeout(() => {
         main_loop()
     }, 1000)
+    resize()
+
+    setInterval(show_next_log_msg, 1000)
 })
 
-const pointerMap = new Map<number, Pointer>()
+window.addEventListener('resize', (ev) => {
+    console.log('window resize')
+    resize()
+})
+
+
+
+let _log = new Array<Array<any>>()
+function show_next_log_msg() {
+    _log.shift()
+}
+const _console_log = console.log
+console.log = (...args: any[]) => {
+    _console_log(...args)
+    _log.push(["log"].concat(args))
+}
+const _console_error = console.error
+console.error = (...args: any[]) => {
+    _console_error(...args)
+    _log.push(["error"].concat(args))
+}
+
+
+
+addEventListener("click", function() {
+    var el: any = document.documentElement,
+      rfs = el.requestFullscreen
+        || el.webkitRequestFullScreen
+        || el.mozRequestFullScreen
+        || el.msRequestFullscreen 
+    ;
+    rfs.call(el);
+});
+
+
+
+function resize() {
+    console.log('resize', window.innerWidth, window.innerHeight)
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    naubino.size.x = canvas.width
+    naubino.size.y = canvas.height
+    mode.set_center_pos()
+}
+
+
+
+function pointer_down(ev: PointerEvent) {
+    canvas.setPointerCapture(ev.pointerId)
+    const pos = pointer_ev_pos(ev)
+    pointer_naubino_touch_down(ev, pos)
+    pointer_down_history(ev, pos)
+    ev.preventDefault()
+}
+function pointer_move(ev: PointerEvent) {
+    const pos = pointer_ev_pos(ev)
+    pointer_naubino_moveTo(ev, pos)
+    pointer_move_history(ev, pos)
+    ev.preventDefault()
+}
+function pointer_up(ev: PointerEvent) {
+    console.log("up", ev.type, ev.which)
+    pointer_naubino_up(ev)
+    pointer_up_history(ev)
+    ev.preventDefault()
+}
+function pointer_ev_pos(ev: PointerEvent) {
+    const rect = canvas.getBoundingClientRect()
+    return {
+        x: -rect.left + ev.x,
+        y: -rect.top + ev.y,
+    }
+}
+
+
+
+const pointerHistoryColors = "red green blue yellow pink purple".split(" ")
+const pointerHistoryMap = new Map<number, Vector[]>()
+function pointer_down_history(ev: PointerEvent, pos: Vector) {
+    pointerHistoryMap.set(ev.pointerId, [pos])
+}
+function pointer_move_history(ev: PointerEvent, pos: Vector) {
+    const history = pointerHistoryMap.get(ev.pointerId)
+    if (history) history.push(pos)
+}
+function pointer_up_history(ev: PointerEvent) {
+    setTimeout(() => pointerHistoryMap.delete(ev.pointerId), 3000)
+}
+function render_pointer_history(ctx: CanvasRenderingContext2D) {
+    ctx.lineWidth = 2
+    for (const [id, history] of pointerHistoryMap.entries()) {
+        ctx.beginPath()
+        ctx.strokeStyle = pointerHistoryColors[id % pointerHistoryColors.length]
+        ctx.moveTo(history[0].x, history[0].y)
+        for (const xy of history.slice(1)) {
+            ctx.lineTo(xy.x, xy.y)
+        }
+        ctx.stroke()
+        ctx.closePath()
+    }
+}
+
+
+
+const pointerNaubinoMap = new Map<number, Pointer>()
+function pointer_naubino_touch_down(ev: PointerEvent, pos: Vector) {
+    const pointer = naubino.touch_down(pos)
+    if (pointer) {
+        pointerNaubinoMap.set(ev.pointerId, pointer)
+    }
+}
+function pointer_naubino_moveTo(ev: PointerEvent, pos: Vector) {
+    const pointer = pointerNaubinoMap.get(ev.pointerId)
+    if (pointer) {
+        pointer.moveTo(pos)
+    }
+}
+function pointer_naubino_up(ev: PointerEvent) {
+    const pointer = pointerNaubinoMap.get(ev.pointerId)
+    if (pointer) {
+        pointer.up()
+        pointerNaubinoMap.delete(ev.pointerId)
+    }
+}
+
+
 
 function canvas_init() {
     canvas = <HTMLCanvasElement>document.querySelector("#naubino42")
@@ -26,29 +154,17 @@ function canvas_init() {
     ctx.fillStyle = "black"
     ctx.fillText("naubino", canvas.width / 2, canvas.height / 2)
 
-    canvas.addEventListener("pointerdown", (ev) => {
-        const rect = canvas.getBoundingClientRect()
-        const pointer = naubino.touch_down({ x: -rect.left + ev.x, y: -rect.top + ev.y })
-        if (pointer) {
-            pointerMap.set(ev.pointerId, pointer)
-        }
-    })
-    canvas.addEventListener("pointermove", (ev) => {
-        const pointer = pointerMap.get(ev.pointerId)
-        if (pointer) {
-            const rect = canvas.getBoundingClientRect()
-            pointer.moveTo({ x: -rect.left + ev.x, y: -rect.top + ev.y })
-        }
-    })
-    canvas.addEventListener("pointerup", (ev) => {
-        const pointer = pointerMap.get(ev.pointerId)
-        if (pointer) {
-            pointer.up()
-            pointerMap.delete(ev.pointerId)
-        }
-    })
+    canvas.addEventListener("pointerdown", pointer_down, false)
+    canvas.addEventListener("pointermove", pointer_move, false)
+    canvas.addEventListener("pointerup", pointer_up, false)
+    canvas.addEventListener("pointercancel", pointer_up, false)
+    canvas.addEventListener("pointerleave", pointer_up, false)
+
     window.addEventListener("keypress", (ev) => {
         if (ev.key == " ") mode.spam_naub_pair()
+    })
+    window.addEventListener("keypress", (ev) => {
+        if (ev.key == "s") mode.spammer = !mode.spammer
     })
 }
 
@@ -56,7 +172,6 @@ function naubino_init() {
     naubino = new Naubino()
     naubino.size = { x: 600, y: 400 }
     mode = new ArenaMode(naubino)
-    mode.spammer = false;
 }
 
 function naubino_start() {
@@ -120,7 +235,15 @@ let naub_anims = new Set<NAnim>()
 function main_loop_render(update: Update) {
     const ctx = canvas.getContext("2d")
     ctx.save()
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    //ctx.fillStyle = "#303030"
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    if (_log[0]) {
+        ctx.fillStyle = "black"
+        const msg = _log[0].join(" ")
+        ctx.fillText(msg, 1, 14)
+    }
 
     ctx.save()
     ctx.strokeStyle = "black"
@@ -188,6 +311,18 @@ function main_loop_render(update: Update) {
     const world = naubino.engine.world
 
     ctx.save()
+    //render_matter()
+    ctx.restore()
+
+    ctx.save()
+    //render_pointer_history(ctx)
+    ctx.restore()
+
+    ctx.restore()
+}
+
+function render_matter(ctx: CanvasRenderingContext2D) {
+    const world = naubino.engine.world
     for (const constraint of world.constraints) {
         const { pointA, pointB, bodyA, bodyB, userData } = constraint
         const xa = pointA.x + (bodyA ? bodyA.position.x : 0)
@@ -222,7 +357,4 @@ function main_loop_render(update: Update) {
         ctx.stroke()
         ctx.restore()
     }
-    ctx.restore()
-
-    ctx.restore()
 }
